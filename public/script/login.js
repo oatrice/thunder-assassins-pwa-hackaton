943557cfda76602c05daa1279a0f310f6dc5e0d7
Worranut Duangkaew <bee@500trends.com>
2017-06-25 16:20:50 +0700

    Store data to Firebase.

--- public/script/login.js
+++ public/script/login.js
@@ -1,20 +1,37 @@
 var googleProvider = new firebase.auth.GoogleAuthProvider();
 var facebookProvider = new firebase.auth.FacebookAuthProvider();
+var database = firebase.database();
+var storage = firebase.storage();
 
 function onSignInGoogleButtonClick() {
     firebase.auth().signInWithPopup(googleProvider).then(function(result) {
         console.log(result);
-        //Do something when login complete
-    }).catch(function(error) {
-        //Do something when error
-    });
+        var user = firebase.auth().currentUser;
+        writeUserData(user.uid, user.displayName, user.email, FirebaseAuth.getInstance().getCurrentUser().getPhotoUrl());
+    }).catch(function(error) {});
 }
 
 function onSignInFacebookButtonClick() {
     firebase.auth().signInWithPopup(facebookProvider).then(function(result) {
         console.log(result);
-        //Do something when login complete
-    }).catch(function(error) {
-        //Do something when error
+        var user = firebase.auth().currentUser;
+        writeUserData(user.uid, user.displayName, user.email, user.photoURL);
+    }).catch(function(error) {});
+}
+
+function writeUserData(userId, name, email, imageUrl) {
+    database.ref('users/' + userId).set({
+        name: name,
+        email: email,
+        profile_picture: imageUrl
+    });
+
+    var storageRef = storage.ref();
+    var imagesRef = storageRef.child('user-image/' + userId);
+
+    var blob = dataURItoBlob(imageUrl);
+
+    imagesRef.put(blob).then(function(snapshot) {
+        console.log(snapshot);
     });
 }
